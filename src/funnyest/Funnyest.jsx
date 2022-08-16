e4fdc5e0223305b13673fbcfd80997cec27a0d34
La'Tonia Mertica <latoniamertica@gmail.com>
2022-08-16 17:28:38 -0400

    funnyest: api output final

--- src/funnyest/Funnyest.jsx
+++ src/funnyest/Funnyest.jsx
@@ -5,7 +5,50 @@ import Box from "@mui/material/Box";
 
 import "./Funnyest.scss";
 
-function Funnyest({}) {
-  <></>;
+const style = {
+  position: "absolute",
+  outline: 0,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "87%",
+  bgcolor: "background.transparent",
+  border: ".05rem solid #fff",
+  boxShadow: 23,
+  p: 1.15,
+
+  color: "#fff",
+  fontFamily: "Arial, Helvetica, sans-serif",
+};
+
+function Funnyest({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Funnyest"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="funnyestCard">
+          <button onClick={onClose}>&#x274C;</button>
+          <div>
+            <fieldset>
+              <p>
+                <select name="funnyest" id="funnyest">
+                  <option value="whoopigoldberg">Whoopi Goldberg</option>
+                  <option value="davidmann">David Mann</option>
+                  <option value="lawandapage">LaWanda Page</option>
+                  <option value="wandasykes">Wanda Sykes</option>
+                  <option value="roberttownsend">Robert Townsend</option>
+                </select>
+              </p>
+            </fieldset>
+            <button type="button">submit</button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Funnyest;
