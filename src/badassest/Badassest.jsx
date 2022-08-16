3d1dc95e710d6d7f3fbea7add02b2f7f4d2f5158
La'Tonia Mertica <latoniamertica@gmail.com>
2022-08-16 17:29:49 -0400

    badassest: semantic filler content created

--- src/badassest/Badassest.jsx
+++ src/badassest/Badassest.jsx
@@ -5,7 +5,72 @@ import Box from "@mui/material/Box";
 
 import "./Badassest.scss";
 
-function Badassest({}) {
-  <></>;
+const style = {
+  position: "absolute",
+  outline: 0,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "87%",
+  bgcolor: "background.transparent",
+  border: ".05rem solid #fff",
+  boxShadow: 23,
+  p: 1.15,
+
+  color: "#fff",
+  fontFamily: "Arial, Helvetica, sans-serif",
+};
+
+function Badassest({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Badassest"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="badassestCard">
+          <button onClick={onClose}>&#x274C;</button>
+          <div>
+            <fieldset className="badassestFieldset">
+              <p className="badassestPara">
+                <input type="radio" name="badassest" id="alice" value="alice" />
+                <label htmlFor="alice">Alice</label>
+                <br />
+                <input type="radio" name="badassest" id="marv" value="marv" />
+                <label htmlFor="marv">Marv</label>
+                <br />
+                <input
+                  type="radio"
+                  name="badassest"
+                  id="johnrambo"
+                  value="johnrambo"
+                />
+                <label htmlFor="johnrambo">John Rambo</label>
+                <br />
+                <input
+                  type="radio"
+                  name="badassest"
+                  id="ellenripley"
+                  value="ellenripley"
+                />
+                <label htmlFor="ellenripley">Ellen Ripley</label>
+                <br />
+                <input
+                  type="radio"
+                  name="badassest"
+                  id="thebride"
+                  value="thebride"
+                />
+                <label htmlFor="thebride">The Bride</label>
+              </p>
+            </fieldset>
+            <button type="button">submit</button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Badassest;
