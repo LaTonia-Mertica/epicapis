5de6786747ba616670422a22d5a54bc7a32f32d0
La'Tonia Mertica <latoniamertica@gmail.com>
2022-08-18 20:21:35 -0400

    pokemon api: working / styles started

--- src/pokemon/Pokemon.jsx
+++ src/pokemon/Pokemon.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
@@ -6,6 +6,10 @@ import Box from "@mui/material/Box";
 import "./Pokemon.scss";
 import axios from "axios";
 
+import pikachuhi from "./images/pikachuhi.gif";
+import { bgcolor } from "@mui/system";
+
+// mui
 const style = {
   position: "absolute",
   outline: 0,
@@ -23,21 +27,27 @@ const style = {
 };
 
 function Pokemon({ openModal, onClose }) {
-  const [poke, setPoke] = useState({
-    poke: "",
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonShow, setPokemonShow] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    type: "",
+    image: "",
   });
 
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/?random/`);
-
-    setPoke({
-      ...poke,
-      poke: data.data.name,
-    });
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/` + pokemonName)
+      .then((response) => {
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          type: response.data.types[0].type.name,
+          image: response.data.sprites.front_default,
+        });
+      });
+    setPokemonShow(true);
   };
 
   return (
@@ -50,8 +60,45 @@ function Pokemon({ openModal, onClose }) {
       >
         <Box sx={style} className="pokemonsCard">
           <button onClick={onClose}>&#x274C;</button>
+          <div>
+            <input
+              type="text"
+              name="pokemonname"
+              id="pokemonname"
+              placeholder="enter pokemon name"
+              autoComplete="off"
+              onChange={(event) => {
+                setPokemonName(event.target.value.toLowerCase());
+              }}
+            />
+          </div>
+          {/* gif credit: tenor.com */}
+          <div>
+            {!pokemonShow ? (
+              <img
+                src={pikachuhi}
+                alt="Pikachu Hi"
+                width="11%"
+                height="7%"
+                className="gif"
+              ></img>
+            ) : (
+              <h1>{pokemonName}</h1>
+            )}
+          </div>
 
-          {poke && <div>{poke.name}</div>}
+          <img src={pokemon.image} alt="Pokemon" className="pokemonImg" />
+          <section>
+            <h2>
+              <span>species: </span>
+              {pokemon.species}
+            </h2>
+            <h2>
+              <span>variety: </span>
+              {pokemon.type}
+            </h2>
+          </section>
+          <Button onClick={searchPokemon}>Search Pokemon</Button>
         </Box>
       </Modal>
     </main>
