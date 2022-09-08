d719ceb508b65b89dac56673a1219a1f8fef7cab
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-08 12:12:37 -0400

    greatest: final greatesters list

--- src/greatest/Greatest.jsx
+++ src/greatest/Greatest.jsx
@@ -35,51 +35,109 @@ function Greatest({ openModal, onClose }) {
           <Button onClick={onClose}>&#x274C;</Button>
           <div>
             <fieldset className="greatestFieldset">
-              <p className="greatestPara">
-                <input
-                  type="checkbox"
-                  name="greatest"
-                  id="aliceinwonderland"
-                  value="aliceinwonderland"
-                />
-                <label htmlFor="aliceinwonderland">Alice in Wonderland</label>
-                <br />
-                <input
-                  type="checkbox"
-                  name="greatest"
-                  id="jasonandtheargonauts"
-                  value="jasonandtheargonauts"
-                />
-                <label htmlFor="jasonandtheargonauts">
-                  Jason & The Argonauts
-                </label>
-                <br />
-                <input
-                  type="checkbox"
-                  name="greatest"
-                  id="panslabyrinth"
-                  value="panslabyrinth"
-                />
-                <label htmlFor="panslabyrinth">Pan's Labyrinth</label>
-                <br />
-                <input
-                  type="checkbox"
-                  name="greatest"
-                  id="theneverendingstory"
-                  value="theneverendingstory"
-                />
-                <label htmlFor="theneverendingstory">
-                  The NeverEnding Story
-                </label>
-                <br />
-                <input
-                  type="checkbox"
-                  name="greatest"
-                  id="theprincessbride"
-                  value="theprincessbride"
-                />
-                <label htmlFor="theprincessbride">The Princess Bride</label>
-              </p>
+              <span>
+                <p className="greatestPara para1">
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="theprincessbride"
+                    value="theprincessbride"
+                  />
+                  <label htmlFor="theprincessbride">The Princess Bride</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="aliceinwonderland"
+                    value="aliceinwonderland"
+                  />
+                  <label htmlFor="aliceinwonderland">Alice in Wonderland</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="legend"
+                    value="legend"
+                  />
+                  <label htmlFor="legend">Legend</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="panslabyrinth"
+                    value="panslabyrinth"
+                  />
+                  <label htmlFor="panslabyrinth">Pan's Labyrinth</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="theneverendingstory"
+                    value="theneverendingstory"
+                  />
+                  <label htmlFor="theneverendingstory">
+                    The Neverending Story
+                  </label>
+                  <br />
+                </p>
+
+                <p className="greatestPara para2">
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="chittychittybangbang"
+                    value="chittychittybangbang"
+                  />
+                  <label htmlFor="chittychittybangbang">
+                    Chitty Chitty Bang Bang
+                  </label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="jasonandtheargonauts"
+                    value="jasonandtheargonauts"
+                  />
+                  <label htmlFor="jasonandtheargonauts">
+                    Jason & The Argonauts
+                  </label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="thewiz"
+                    value="thewiz"
+                  />
+                  <label htmlFor="thewiz">The Wiz</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="thedarkcrystal"
+                    value="thedarkcrystal"
+                  />
+                  <label htmlFor="thedarkcrystal">The Dark Crystal</label>
+                  <br />
+
+                  <input
+                    type="checkbox"
+                    name="greatest"
+                    id="whoframedrogerrabbit"
+                    value="whoframedrogerrabbit"
+                  />
+                  <label htmlFor="whoframedrogerrabbit">
+                    Who Framed Roger Rabbit
+                  </label>
+                  <br />
+                </p>
+              </span>
             </fieldset>
             <Button type="button" className="submitBtn">
               submit
