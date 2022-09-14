833b6f3c4bb08e3916fc57c47cd5fa18d310be94
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-13 22:17:30 -0400

    rampantest: building semantic filler as select / input combo

--- src/rampantest/Rampantest.jsx
+++ src/rampantest/Rampantest.jsx
@@ -1,11 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
 import "./Rampantest.scss";
 
-function Rampantest({}) {
-  <></>;
+// mui
+const style = {
+  position: "absolute",
+  outline: 0,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "87%",
+  bgcolor: "background.transparent",
+  border: ".05rem solid #fff",
+  boxShadow: 23,
+  p: 1.15,
+
+  color: "#fff",
+  fontFamily: "Arial, Helvetica, sans-serif",
+};
+
+function Rampantest({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Rampantest"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="rampantestCard">
+          <Button onClick={onClose}>&#x274C;</Button>
+          <div>
+            <fieldset>
+              <p className="rampantestPara">
+                <label htmlFor="decade">Rampantest Decade:&nbsp;&nbsp;</label>
+                <input
+                  type="number"
+                  name="decade"
+                  id="decade"
+                  min="1900"
+                  max="2020"
+                  step="10"
+                  className="decade"
+                  placeholder="select"
+                />
+                <input
+                  type="text"
+                  name="rampantestEntry"
+                  id="rampantestEntry"
+                  placeholder="if not hercule poirot, then who .. ."
+                  className="rampantestEntry"
+                />
+              </p>
+            </fieldset>
+            <button type="button" className="submitBtn">
+              submit
+            </button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Rampantest;
