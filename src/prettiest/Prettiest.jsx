6c754b64c64aac88b721274454899c7e4ce304cf
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-13 22:14:51 -0400

    prettiest: building semantic filler as radio variation

--- src/prettiest/Prettiest.jsx
+++ src/prettiest/Prettiest.jsx
@@ -1,11 +1,112 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
 import "./Prettiest.scss";
 
-function Prettiest({}) {
-  <></>;
+// mui
+const style = {
+  position: "absolute",
+  outline: 0,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "87%",
+  bgcolor: "background.transparent",
+  border: ".05rem solid #fff",
+  boxShadow: 23,
+  p: 1.15,
+
+  color: "#fff",
+  fontFamily: "Arial, Helvetica, sans-serif",
+};
+
+function Prettiest({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Prettiest"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="prettiestCard">
+          <Button onClick={onClose}>&#x274C;</Button>
+          <div className="radioFieldsetDiv">
+            <fieldset className="prettiestFieldset">
+              <p className="prettiestPara">
+                <input
+                  type="radio"
+                  name="prettiest"
+                  id="highball"
+                  value="highball"
+                />
+                <label htmlFor="highball">
+                  High Ball
+                  <br />
+                  <span className="pretties">
+                    anything <i>dark & stormy</i>
+                  </span>
+                </label>
+                <br />
+
+                <input type="radio" name="prettiest" id="coupe" value="coupe" />
+                <label htmlFor="coupe">
+                  Coupe
+                  <br />
+                  <span className="pretties">
+                    necessarily <i>boulevardier</i>
+                  </span>
+                </label>
+                <br />
+
+                <input
+                  type="radio"
+                  name="prettiest"
+                  id="zombie"
+                  value="zombie"
+                />
+                <label htmlFor="zombie">
+                  Zombie <br />
+                  <span className="pretties">
+                    think <i>tequila sunrise</i>
+                  </span>
+                </label>
+                <br />
+
+                <input type="radio" name="prettiest" id="rocks" value="rocks" />
+                <label htmlFor="rocks">
+                  Rocks <br />
+                  <span className="pretties">
+                    essentially <i>old fashioned</i>
+                  </span>
+                </label>
+                <br />
+
+                <input
+                  type="radio"
+                  name="prettiest"
+                  id="hurricane"
+                  value="hurricane"
+                />
+                <label htmlFor="hurricane">
+                  Hurricane
+                  <br />
+                  <span className="pretties">
+                    akin <i>singapore sling</i>
+                  </span>
+                </label>
+                <br />
+              </p>
+            </fieldset>
+            <button type="button" className="submitBtn">
+              submit
+            </button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Prettiest;
