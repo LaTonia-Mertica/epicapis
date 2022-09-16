c679f6f6ebe19bd0a77ee52ee1594c513aebe693
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-15 22:23:28 -0400

    beautiful / dangerous code: basic structure and styles

--- src/beautiful/Beautiful.jsx
+++ src/beautiful/Beautiful.jsx
@@ -1,9 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
 import "./Beautiful.scss";
+import appropriate from "./images/appropriate.png";
+import bold from "./images/bold.png";
+import clean from "./images/clean.png";
+import clear from "./images/clear.png";
+import comfy from "./images/comfy.png";
+
+import consistent from "./images/consistent.png";
+import efficient from "./images/efficient.png";
+import elegant from "./images/elegant.png";
+import fun from "./images/fun.png";
+import functional from "./images/functional.png";
+import modular from "./images/modular.png";
+import sensible from "./images/sensible.png";
+import simple from "./images/simple.png";
+import testseasy from "./images/tests-easy.png";
 
 // mui
 const style = {
@@ -22,7 +37,45 @@ const style = {
   fontFamily: "Arial, Helvetica, sans-serif",
 };
 
-function Beautiful({}) {
-  <></>;
+function Beautiful({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Beautiful"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="beautifulCard">
+          <Button onClick={onClose}>&#x274C;</Button>
+          <div>
+            <section>
+              <img src={appropriate} alt="" width="13%" height="auto" />
+              <img src={bold} alt="" width="13%" height="auto" />
+              <img src={clean} alt="" width="13%" height="auto" />
+              <img src={clear} alt="" width="13%" height="auto" />
+              <img src={comfy} alt="" width="13%" height="auto" />
+              <img src={consistent} alt="" width="13%" height="auto" />
+              <img src={efficient} alt="" width="13%" height="auto" />
+            </section>
+
+            <section>
+              <img src={elegant} alt="" width="13%" height="auto" />
+              <img src={fun} alt="" width="13%" height="auto" />
+              <img src={functional} alt="" width="13%" height="auto" />
+              <img src={modular} alt="" width="13%" height="auto" />
+              <img src={sensible} alt="" width="13%" height="auto" />
+              <img src={simple} alt="" width="13%" height="auto" />
+              <img src={testseasy} alt="" width="13%" height="auto" />
+            </section>
+
+            <Button type="button" className="submitBtn">
+              submit
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Beautiful;
--- src/dangerous/Dangerous.jsx
+++ src/dangerous/Dangerous.jsx
@@ -1,9 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 
 import "./Dangerous.scss";
+import alienating from "./images/alienating.png";
+import boastful from "./images/boastful.png";
+import boring from "./images/boring.png";
+import chaotic from "./images/chaotic.png";
+import cluttered from "./images/cluttered.png";
+import disfunctional from "./images/disfunctional.png";
+import limiting from "./images/limiting.png";
+import malicious from "./images/malicious.png";
+import misleading from "./images/misleading.png";
+import slow from "./images/slow.png";
+import toonested from "./images/too-nested.png";
+import uninspiring from "./images/uninspiring.png";
+import unrelatable from "./images/unrelatable.png";
+import unwieldy from "./images/unwieldy.png";
 
 // mui
 const style = {
@@ -22,7 +36,44 @@ const style = {
   fontFamily: "Arial, Helvetica, sans-serif",
 };
 
-function Dangerous({}) {
-  <></>;
+function Dangerous({ openModal, onClose }) {
+  return (
+    <main>
+      <Modal
+        open={openModal === "Dangerous"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="dangerousCard">
+          <Button onClick={onClose}>&#x274C;</Button>
+          <div>
+            <section>
+              <img src={alienating} alt="" width="13%" height="auto" />
+              <img src={boastful} alt="" width="13%" height="auto" />
+              <img src={boring} alt="" width="13%" height="auto" />
+              <img src={chaotic} alt="" width="13%" height="auto" />
+              <img src={cluttered} alt="" width="13%" height="auto" />
+              <img src={disfunctional} alt="" width="13%" height="auto" />
+              <img src={limiting} alt="" width="13%" height="auto" />
+            </section>
+
+            <section>
+              <img src={malicious} alt="" width="13%" height="auto" />
+              <img src={misleading} alt="" width="13%" height="auto" />
+              <img src={slow} alt="" width="13%" height="auto" />
+              <img src={toonested} alt="" width="13%" height="auto" />
+              <img src={uninspiring} alt="" width="13%" height="auto" />
+              <img src={unrelatable} alt="" width="13%" height="auto" />
+              <img src={unwieldy} alt="" width="13%" height="auto" />
+            </section>
+            <Button type="button" className="submitBtn">
+              submit
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default Dangerous;
