56129ef8f0138d5a7dba934a306e1b0b4f68a921
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-16 22:26:22 -0400

    ron swanson: wired

--- src/ron swanson/RonSwanson.jsx
+++ src/ron swanson/RonSwanson.jsx
@@ -22,7 +22,35 @@ const style = {
   fontFamily: "Arial, Helvetica, sans-serif",
 };
 
-function RonSwanson({}) {
-  <></>;
+function RonSwanson({ openModal, onClose }) {
+  const [quote, setQuote] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch(
+        `https://ron-swanson-quotes.herokuapp.com/v2/quotes`
+      );
+      const json = await data.json();
+      setQuote(json[0]);
+    })();
+  }, []);
+
+  return (
+    <main>
+      <Modal
+        open={openModal === "RonSwanson"}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="ronsCard">
+          <Button onClick={onClose}>&#x274C;</Button>
+          <div>
+            <p>{quote}</p>
+          </div>
+        </Box>
+      </Modal>
+    </main>
+  );
 }
 export default RonSwanson;
