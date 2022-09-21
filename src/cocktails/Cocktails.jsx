e9c50af5a1fec10311738a8fb67178c7eb8b76ab
La'Tonia Mertica <latoniamertica@gmail.com>
2022-09-21 14:37:30 -0400

    cocktails: code clean / removed axios import

--- src/cocktails/Cocktails.jsx
+++ src/cocktails/Cocktails.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import axios from "axios";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
