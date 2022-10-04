cb79921eaebf8924908a9d6e63c7480c97ad5ef2
La'Tonia Mertica <latoniamertica@gmail.com>
2022-10-04 14:14:24 -0400

    swanson: manly mode added w/text effect

--- src/ron swanson/RonSwanson.jsx
+++ src/ron swanson/RonSwanson.jsx
@@ -7,7 +7,28 @@ import "./RonSwanson.scss";
 import { style } from "../mui.js";
 
 function RonSwanson({ openModal, onClose }) {
+  const [manlyMode, setManlyMode] = React.useState(false);
   const [quote, setQuote] = useState();
+
+  React.useEffect(() => {
+    const json = localStorage.getItem("site-manly-mode");
+    const currentMode = JSON.parse(json);
+    if (currentMode) {
+      setManlyMode(true);
+    } else {
+      setManlyMode(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (manlyMode) {
+      document.body.classList.add("manly");
+    } else {
+      document.body.classList.remove("manly");
+    }
+    const json = JSON.stringify(manlyMode);
+    localStorage.setItem("site-manly-mode", json);
+  }, [manlyMode]);
 
   useEffect(() => {
     (async () => {
@@ -31,7 +52,16 @@ function RonSwanson({ openModal, onClose }) {
           <Button onClick={onClose}>&#x1F167;</Button>
 
           <div>
-            <p>'{quote}'</p>
+            <p>
+              '{quote}'
+              <br />
+              <button
+                onClick={() => setManlyMode(!manlyMode)}
+                className="manlyModeBtn lineUpEffect"
+              >
+                MANLY MODE&nbsp;&nbsp;|&nbsp;&nbsp;2 VIEWS
+              </button>
+            </p>
           </div>
         </Box>
       </Modal>
