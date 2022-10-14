3b90aa19ee17830e8bbafe71e67834305d0361b1
La'Tonia Mertica <latoniamertica@gmail.com>
2022-10-14 16:27:38 -0400

    pokemon: created file for gifs, imported gif file, added gifs

--- src/pokemon/Pokemon.jsx
+++ src/pokemon/Pokemon.jsx
@@ -6,55 +6,14 @@ import Button from "@mui/material/Button";
 
 import "./Pokemon.scss";
 import { style } from "../mui.js";
-
-import chansey from "./images/chansey.gif";
-import charizardxflying from "./images/charizardxflying.gif";
-import charmeleon from "./images/charmeleon.gif";
-import cinderace from "./images/cinderace.gif";
-import djvulpix from "./images/djvulpix.gif";
-import eevee from "./images/eevee.gif";
-import froakie from "./images/froakie.gif";
-import furret from "./images/furret.gif";
-import garchomp from "./images/garchomp.gif";
-import lucario from "./images/lucario.gif";
-import mewtwo from "./images/mewtwo.gif";
-import nintendo from "./images/nintendo.gif";
-import pikachuhi from "./images/pikachuhi.gif";
-import pikachulove from "./images/pikachulove.gif";
-import pikachumusic from "./images/pikachumusic.gif";
-import squirtle from "./images/squirtle.gif";
-import sylveon from "./images/sylveon.gif";
-import togedemaru from "./images/togedemaru.gif";
-import venomoth from "./images/venomoth.gif";
-import zoroa from "./images/zoroa.gif";
-
-const pokemonGifs = [
-  <img src={chansey} alt="Pokemon" />,
-  <img src={charizardxflying} alt="Pokemon" />,
-  <img src={charmeleon} alt="Pokemon" />,
-  <img src={cinderace} alt="Pokemon" />,
-  <img src={djvulpix} alt="Pokemon" />,
-  <img src={eevee} alt="Pokemon" />,
-  <img src={froakie} alt="Pokemon" />,
-  <img src={furret} alt="Pokemon" />,
-  <img src={garchomp} alt="Pokemon" />,
-  <img src={lucario} alt="Pokemon" />,
-  <img src={mewtwo} alt="Pokemon" />,
-  <img src={nintendo} alt="Pokemon" />,
-  <img src={pikachuhi} alt="Pokemon" />,
-  <img src={pikachulove} alt="Pokemon" />,
-  <img src={pikachumusic} alt="Pokemon" />,
-  <img src={squirtle} alt="Pokemon" />,
-  <img src={sylveon} alt="Pokemon" />,
-  <img src={togedemaru} alt="Pokemon" />,
-  <img src={venomoth} alt="Pokemon" />,
-  <img src={zoroa} alt="Pokemon" />,
-];
-
-/* gif credit: tenor.com */
+import pokemonGifs from "./pokemonGifs";
 
 let randomGif = Math.floor(Math.random() * (pokemonGifs.length + 1));
 randomGif = pokemonGifs[randomGif];
+
+for (const pokemon in pokemonGifs) {
+  console.log(pokemon);
+}
 
 const Pokemon = ({ openModal, onClose }) => {
   const [pokemonName, setPokemonName] = useState("");
---
+++ src/pokemon/pokemonGifs.js
@@ -0,0 +1,300 @@
+/* gif credit: tenor.com */
+
+import alolanvulpix from "./images/alolanvulpix.gif";
+import articuno from "./images/articuno.gif";
+import barboach from "./images/barboach.gif";
+import bidoof from "./images/bidoof.gif";
+import bidoofteeth from "./images/bidoofteeth.gif";
+import braixen from "./images/braixen.gif";
+import bronzonshiny from "./images/bronzonshiny.gif";
+import bulbasaurgreen from "./images/bulbasaurgreen.gif";
+import bulbasaurtwist from "./images/bulbasaurtwist.gif";
+import castform from "./images/castform.gif";
+import catleopard from "./images/catleopard.gif";
+import cerneaf from "./images/cerneaf.gif";
+import chansey from "./images/chansey.gif";
+import charmeleon from "./images/charmeleon.gif";
+import charizardxflying from "./images/charizardxflying.gif";
+import charizardromp from "./images/charizardromp.gif";
+import charizardscreech from "./images/charizardscreech.gif";
+import charmander from "./images/charmander.gif";
+import charmanderbbE from "./images/charmanderbbE.gif";
+import cinderace from "./images/cinderace.gif";
+import chikorita from "./images/chikorita.gif";
+import crazyeyesrun from "./images/crazyeyesrun.gif";
+import croagunk from "./images/croagunk.gif";
+import cubone from "./images/cubone.gif";
+import ditto from "./images/ditto.gif";
+import djvulpix from "./images/djvulpix.gif";
+import dreepy from "./images/dreepy.gif";
+import eevee from "./images/eevee.gif";
+import eeveecute from "./images/eeveecute.gif";
+import eeveedance from "./images/eeveedance.gif";
+import eeveeflareon from "./images/eeveeflareon.gif";
+import eeveeglaceon from "./images/eeveeglaceon.gif";
+import eeveelution from "./images/eeveelution.gif";
+import eeveeperplex from "./images/eeveeperplex.gif";
+import eeveerant from "./images/eeveerant.gif";
+import eeveesleep from "./images/eeveesleep.gif";
+import eeveetriplestep from "./images/eeveetriplestep.gif";
+import eeveetwostep from "./images/eeveetwostep.gif";
+import electabuzz from "./images/electabuzz.gif";
+import elekid from "./images/elekid.gif";
+import espeon from "./images/espeon.gif";
+import espeondj from "./images/espeondj.gif";
+import fennekinears from "./images/fennekinears.gif";
+import fennekinsprint from "./images/fennekinsprint.gif";
+import flygon from "./images/flygon.gif";
+import frikoid from "./images/frikoid.gif";
+import froakie from "./images/froakie.gif";
+import furret from "./images/furret.gif";
+import furrethappy from "./images/furrethappy.gif";
+import froakielazzy from "./images/froakielazzy.gif";
+import galar from "./images/galar.gif";
+import garchomp from "./images/garchomp.gif";
+import glaceondj from "./images/glaceondj.gif";
+import glaceonspin from "./images/glaceonspin.gif";
+import glaceontail from "./images/glaceontail.gif";
+import glaceonyelp from "./images/glaceonyelp.gif";
+import glameow from "./images/glameow.gif";
+import gossifleurcolor from "./images/gossifleurcolor.gif";
+import gossifleureldegoss from "./images/gossifleureldegoss.gif";
+import heracross from "./images/heracross.gif";
+import jolteon from "./images/jolteon.gif";
+import kirlia from "./images/kirlia.gif";
+import larvitar from "./images/larvitar.gif";
+import larvitarsit from "./images/larvitarsit.gif";
+import leafeon from "./images/leafeon.gif";
+import lickitung from "./images/lickitung.gif";
+import lickitunglick from "./images/lickitunglick.gif";
+import litleo from "./images/litleo.gif";
+import lopunnymega from "./images/lopunnymega.gif";
+import lucario from "./images/lucario.gif";
+import lucarioflap from "./images/lucarioflap.gif";
+import lucariokick from "./images/lucariokick.gif";
+import ludicolo from "./images/ludicolo.gif";
+import magikarp from "./images/magikarp.gif";
+import magmarfrowning from "./images/magmarfrowning.gif";
+import masterball from "./images/masterball.gif";
+import megalopunny from "./images/megalopunny.gif";
+import mewtwo from "./images/mewtwo.gif";
+import meowth from "./images/meowth.gif";
+import meowthswat from "./images/meowthswat.gif";
+import mew from "./images/mew.gif";
+import mewspin from "./images/mewspin.gif";
+import mewtwoshiny from "./images/mewtwoshiny.gif";
+import miltank from "./images/miltank.gif";
+import murkrow from "./images/murkrow.gif";
+import natuneetoe from "./images/natuneetoe.gif";
+import nintendo from "./images/nintendo.gif";
+import nintendostance from "./images/nintendostance.gif";
+import nonono from "./images/nonono.gif";
+import octilleryswim from "./images/octilleryswim.gif";
+import omanyte from "./images/omanyte.gif";
+import oshawott from "./images/oshawott.gif";
+import owlrowlet from "./images/owlrowlet.gif";
+import panchampunch from "./images/panchampunch.gif";
+import panchamshiny from "./images/panchamshiny.gif";
+import pikachuhi from "./images/pikachuhi.gif";
+import pikachulove from "./images/pikachulove.gif";
+import pikachumusic from "./images/pikachumusic.gif";
+import pikachubabe from "./images/pikachubabe.gif";
+import pikachudaze from "./images/pikachudaze.gif";
+import pikachupower from "./images/pikachupower.gif";
+import pikachuroll from "./images/pikachuroll.gif";
+import pikachuromp from "./images/pikachuromp.gif";
+import pikachurun from "./images/pikachurun.gif";
+import pikachutiptoe from "./images/pikachutiptoe.gif";
+import pikachutriggerfingers from "./images/pikachutriggerfingers.gif";
+import pikachuyeah from "./images/pikachuyeah.gif";
+import poliwag from "./images/poliwag.gif";
+import poochyena from "./images/poochyena.gif";
+import pumpkaboo from "./images/pumpkaboo.gif";
+import raichuhelicopter from "./images/raichuhelicopter.gif";
+import ralts from "./images/ralts.gif";
+import regice from "./images/regice.gif";
+import rockruffblush from "./images/rockruffblush.gif";
+import roselia from "./images/roselia.gif";
+import rotomthink from "./images/rotomthink.gif";
+import ruffletbirdshiny from "./images/ruffletbirdshiny.gif";
+import scolipede from "./images/scolipede.gif";
+import scorbunnygalar from "./images/scorbunnygalar.gif";
+import shelgon from "./images/shelgon.gif";
+import shinxfrikoid from "./images/shinxfrikoid.gif";
+import showkemon from "./images/showkemon.gif";
+import shroomishside from "./images/shroomishside.gif";
+import sirfetchshiny from "./images/sirfetchshiny.gif";
+import skitty from "./images/skitty.gif";
+import sneasel from "./images/sneasel.gif";
+import snorlax from "./images/snorlax.gif";
+import snorlaxromp from "./images/snorlaxromp.gif";
+import softpokemon from "./images/softpokemon.gif";
+import squirtle from "./images/squirtle.gif";
+import squirtlehug from "./images/squirtlehug.gif";
+import sylveon from "./images/sylveon.gif";
+import togedemaru from "./images/togedemaru.gif";
+import trippy from "./images/trippy.gif";
+import tyranitar from "./images/tyranitar.gif";
+import umbreondefense from "./images/umbreondefense.gif";
+import umbreondj from "./images/umbreondj.gif";
+import vapoeronspin from "./images/vapoeronspin.gif";
+import venomoth from "./images/venomoth.gif";
+import venusaur from "./images/venusaur.gif";
+import vespiquen from "./images/vespiquen.gif";
+import vulpixdoe from "./images/vulpixdoe.gif";
+import vulpixfire from "./images/vulpixfire.gif";
+import vulpixshine from "./images/vulpixshine.gif";
+import wobbuffet from "./images/wobbuffet.gif";
+import wooper from "./images/wooper.gif";
+import zoroa from "./images/zoroa.gif";
+
+const pokemonGifs = [
+  <img src={alolanvulpix} alt="Pokemon" />,
+  <img src={articuno} alt="Pokemon" />,
+  <img src={barboach} alt="Pokemon" />,
+  <img src={bidoof} alt="Pokemon" />,
+  <img src={bidoofteeth} alt="Pokemon" />,
+  <img src={braixen} alt="Pokemon" />,
+  <img src={bronzonshiny} alt="Pokemon" />,
+  <img src={bulbasaurgreen} alt="Pokemon" />,
+  <img src={bulbasaurtwist} alt="Pokemon" />,
+  <img src={castform} alt="Pokemon" />,
+  <img src={catleopard} alt="Pokemon" />,
+  <img src={cerneaf} alt="Pokemon" />,
+  <img src={chansey} alt="Pokemon" />,
+  <img src={charizardxflying} alt="Pokemon" />,
+  <img src={charizardromp} alt="Pokemon" />,
+  <img src={charizardscreech} alt="Pokemon" />,
+  <img src={charmander} alt="Pokemon" />,
+  <img src={charmanderbbE} alt="Pokemon" />,
+  <img src={charmeleon} alt="Pokemon" />,
+  <img src={chikorita} alt="Pokemon" />,
+  <img src={cinderace} alt="Pokemon" />,
+  <img src={crazyeyesrun} alt="Pokemon" />,
+  <img src={croagunk} alt="Pokemon" />,
+  <img src={cubone} alt="Pokemon" />,
+  <img src={ditto} alt="Pokemon" />,
+  <img src={djvulpix} alt="Pokemon" />,
+  <img src={dreepy} alt="Pokemon" />,
+  <img src={eevee} alt="Pokemon" />,
+  <img src={eeveecute} alt="Pokemon" />,
+  <img src={eeveedance} alt="Pokemon" />,
+  <img src={eeveeflareon} alt="Pokemon" />,
+  <img src={eeveeglaceon} alt="Pokemon" />,
+  <img src={eeveelution} alt="Pokemon" />,
+  <img src={eeveeperplex} alt="Pokemon" />,
+  <img src={eeveerant} alt="Pokemon" />,
+  <img src={eeveesleep} alt="Pokemon" />,
+  <img src={eeveetriplestep} alt="Pokemon" />,
+  <img src={eeveetwostep} alt="Pokemon" />,
+  <img src={electabuzz} alt="Pokemon" />,
+  <img src={elekid} alt="Pokemon" />,
+  <img src={espeon} alt="Pokemon" />,
+  <img src={espeondj} alt="Pokemon" />,
+  <img src={fennekinears} alt="Pokemon" />,
+  <img src={fennekinsprint} alt="Pokemon" />,
+  <img src={flygon} alt="Pokemon" />,
+  <img src={frikoid} alt="Pokemon" />,
+  <img src={froakie} alt="Pokemon" />,
+  <img src={furrethappy} alt="Pokemon" />,
+  <img src={froakielazzy} alt="Pokemon" />,
+  <img src={furret} alt="Pokemon" />,
+  <img src={galar} alt="Pokemon" />,
+  <img src={garchomp} alt="Pokemon" />,
+  <img src={glaceondj} alt="Pokemon" />,
+  <img src={glaceonspin} alt="Pokemon" />,
+  <img src={glaceontail} alt="Pokemon" />,
+  <img src={glaceonyelp} alt="Pokemon" />,
+  <img src={glameow} alt="Pokemon" />,
+  <img src={gossifleurcolor} alt="Pokemon" />,
+  <img src={gossifleureldegoss} alt="Pokemon" />,
+  <img src={heracross} alt="Pokemon" />,
+  <img src={jolteon} alt="Pokemon" />,
+  <img src={kirlia} alt="Pokemon" />,
+  <img src={larvitar} alt="Pokemon" />,
+  <img src={larvitarsit} alt="Pokemon" />,
+  <img src={leafeon} alt="Pokemon" />,
+  <img src={lickitung} alt="Pokemon" />,
+  <img src={lickitunglick} alt="Pokemon" />,
+  <img src={litleo} alt="Pokemon" />,
+  <img src={lopunnymega} alt="Pokemon" />,
+  <img src={lucario} alt="Pokemon" />,
+  <img src={lucarioflap} alt="Pokemon" />,
+  <img src={lucariokick} alt="Pokemon" />,
+  <img src={ludicolo} alt="Pokemon" />,
+  <img src={magikarp} alt="Pokemon" />,
+  <img src={magmarfrowning} alt="Pokemon" />,
+  <img src={masterball} alt="Pokemon" />,
+  <img src={megalopunny} alt="Pokemon" />,
+  <img src={meowth} alt="Pokemon" />,
+  <img src={meowthswat} alt="Pokemon" />,
+  <img src={mew} alt="Pokemon" />,
+  <img src={mewspin} alt="Pokemon" />,
+  <img src={mewtwo} alt="Pokemon" />,
+  <img src={mewtwoshiny} alt="Pokemon" />,
+  <img src={miltank} alt="Pokemon" />,
+  <img src={murkrow} alt="Pokemon" />,
+  <img src={natuneetoe} alt="Pokemon" />,
+  <img src={nintendo} alt="Pokemon" />,
+  <img src={nintendostance} alt="Pokemon" />,
+  <img src={nonono} alt="Pokemon" />,
+  <img src={octilleryswim} alt="Pokemon" />,
+  <img src={omanyte} alt="Pokemon" />,
+  <img src={oshawott} alt="Pokemon" />,
+  <img src={owlrowlet} alt="Pokemon" />,
+  <img src={panchampunch} alt="Pokemon" />,
+  <img src={panchamshiny} alt="Pokemon" />,
+  <img src={pikachubabe} alt="Pokemon" />,
+  <img src={pikachudaze} alt="Pokemon" />,
+  <img src={pikachuhi} alt="Pokemon" />,
+  <img src={pikachulove} alt="Pokemon" />,
+  <img src={pikachumusic} alt="Pokemon" />,
+  <img src={pikachupower} alt="Pokemon" />,
+  <img src={pikachuroll} alt="Pokemon" />,
+  <img src={pikachuromp} alt="Pokemon" />,
+  <img src={pikachurun} alt="Pokemon" />,
+  <img src={pikachutiptoe} alt="Pokemon" />,
+  <img src={pikachutriggerfingers} alt="Pokemon" />,
+  <img src={pikachuyeah} alt="Pokemon" />,
+  <img src={poliwag} alt="Pokemon" />,
+  <img src={poochyena} alt="Pokemon" />,
+  <img src={pumpkaboo} alt="Pokemon" />,
+  <img src={raichuhelicopter} alt="Pokemon" />,
+  <img src={ralts} alt="Pokemon" />,
+  <img src={regice} alt="Pokemon" />,
+  <img src={rockruffblush} alt="Pokemon" />,
+  <img src={roselia} alt="Pokemon" />,
+  <img src={rotomthink} alt="Pokemon" />,
+  <img src={ruffletbirdshiny} alt="Pokemon" />,
+  <img src={scolipede} alt="Pokemon" />,
+  <img src={scorbunnygalar} alt="Pokemon" />,
+  <img src={shelgon} alt="Pokemon" />,
+  <img src={shinxfrikoid} alt="Pokemon" />,
+  <img src={showkemon} alt="Pokemon" />,
+  <img src={shroomishside} alt="Pokemon" />,
+  <img src={sirfetchshiny} alt="Pokemon" />,
+  <img src={skitty} alt="Pokemon" />,
+  <img src={sneasel} alt="Pokemon" />,
+  <img src={snorlax} alt="Pokemon" />,
+  <img src={snorlaxromp} alt="Pokemon" />,
+  <img src={softpokemon} alt="Pokemon" />,
+  <img src={squirtle} alt="Pokemon" />,
+  <img src={squirtlehug} alt="Pokemon" />,
+  <img src={sylveon} alt="Pokemon" />,
+  <img src={togedemaru} alt="Pokemon" />,
+  <img src={trippy} alt="Pokemon" />,
+  <img src={tyranitar} alt="Pokemon" />,
+  <img src={umbreondefense} alt="Pokemon" />,
+  <img src={umbreondj} alt="Pokemon" />,
+  <img src={vapoeronspin} alt="Pokemon" />,
+  <img src={venomoth} alt="Pokemon" />,
+  <img src={venusaur} alt="Pokemon" />,
+  <img src={vespiquen} alt="Pokemon" />,
+  <img src={vulpixdoe} alt="Pokemon" />,
+  <img src={vulpixfire} alt="Pokemon" />,
+  <img src={vulpixshine} alt="Pokemon" />,
+  <img src={wobbuffet} alt="Pokemon" />,
+  <img src={wooper} alt="Pokemon" />,
+  <img src={zoroa} alt="Pokemon" />,
+];
+export default pokemonGifs;
