168db35dcbfb5b2785382ee5ac4ad9f042642af4
La'Tonia Mertica <latoniamertica@gmail.com>
2022-10-28 12:10:58 -0400

    pokemon: refactor / replaced search with auto complete from api call

--- src/pokemon/Pokemon.jsx
+++ src/pokemon/Pokemon.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Select from "react-select";
 
 import "./Pokemon.scss";
 import { style } from "../mui.js";
@@ -10,7 +11,7 @@ import pokemonGifs from "./pokemonGifs";
 import pokemonworldmap from "./images/pokemonworldmap.png";
 
 const Pokemon = ({ openModal, onClose }) => {
-  const [pokemonName, setPokemonName] = useState("");
+  // const [pokemonName, setPokemonName] = useState("");
   const [pokemonShow, setPokemonShow] = useState(false);
   const [pokemon, setPokemon] = useState({
     name: "",
@@ -27,14 +28,15 @@ const Pokemon = ({ openModal, onClose }) => {
   const [pokemonImage, setPokemonImage] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const [allPokemon, setAllPokemon] = useState([]);
 
-  const searchPokemon = (event) => {
-    event.preventDefault();
+  const searchPokemon = (pokemonName) => {
     setLoading(true);
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/` + pokemonName)
       .then((response) => {
         if (response?.data?.species?.name) {
+          console.log(response.data);
           setPokemon({
             name: pokemonName,
             species: response.data.species.name,
@@ -43,10 +45,13 @@ const Pokemon = ({ openModal, onClose }) => {
             image_back: response.data.sprites.back_default,
             back_shiny: response.data.sprites.back_shiny,
             front_shiny: response.data.sprites.front_shiny,
-            abilities: [
-              response.data.abilities[0].ability.name,
-              response.data.abilities[1].ability.name,
-            ],
+            abilities:
+              response.data?.abilities.length > 1
+                ? [
+                    response.data.abilities[0].ability.name,
+                    response.data.abilities[1].ability.name,
+                  ]
+                : [response.data.abilities[0].ability.name],
             moves: [
               response.data.moves[0].move.name,
               response.data.moves[1].move.name,
@@ -91,6 +96,11 @@ const Pokemon = ({ openModal, onClose }) => {
       });
   };
 
+  // **note:** reasons this api uses axios:
+  // axios and fetch are used in the epic apis project for practice with both libraries
+  // asycn/await and .then are both used in the epic apis project for familiarity with both types of asynchronous code syntax
+  // fetch is the more modern and preferred method
+
   const getGifMatch = () => {
     let randomGifMatch = Math.floor(Math.random() * pokemonGifs.length);
     let gifMatch = pokemonGifs[randomGifMatch];
@@ -101,13 +111,25 @@ const Pokemon = ({ openModal, onClose }) => {
     if (openModal) {
       setPokemonImage(getGifMatch());
       setPokemonShow(false);
+
+      // TODO 10/25/22 MZM: This is for our autocomplete code, but we need to
+      //refactor to use the npm react-select package
+      if (allPokemon.length === 0) {
+        axios
+          .get(`https://pokeapi.co/api/v2/pokemon/?limit=2000`)
+          .then((response) => {
+            const pokemonData = response.data.results;
+
+            setAllPokemon(
+              pokemonData.map((pokemon) => ({
+                value: pokemon.name,
+                label: pokemon.name,
+              }))
+            );
+          });
+      }
     }
   }, [openModal]);
-
-  // **note:** reasons this api uses axios:
-  // axios and fetch are used in the epic apis project for practice with both libraries
-  // asycn/await and .then are both used in the epic apis project for familiarity with both types of asynchronous code syntax
-  // fetch is the more modern and preferred method
 
   return (
     <main id="pokemon">
@@ -120,19 +142,32 @@ const Pokemon = ({ openModal, onClose }) => {
         <Box sx={style} className="pokemonsCard">
           <button onClick={onClose}>&#x24B3;</button>
           <form onSubmit={searchPokemon}>
-            <input
+            {/* <input
               type="text"
               name="pokemonname"
               id="pokemonname"
               placeholder="enter pokemon name"
-              autoComplete="on"
+              autoComplete="off"
               onChange={(event) => {
                 setPokemonName(event.target.value.toLowerCase());
               }}
+            /> */}
+            <Select
+              options={allPokemon}
+              styles={{
+                option: () => ({
+                  color: "black",
+                }),
+              }}
+              onChange={(newValue) => {
+                searchPokemon(newValue.value);
+              }}
+              openMenuOnFocus={false}
+              openMenuOnClick={false}
             />
 
-            <Button type="submit" onClick={searchPokemon} className="searchBtn">
-              {loading ? <>loading...</> : <> Search Pokemon</>}
+            <Button disabled={true} className="searchBtn">
+              {loading ? <>loading...</> : ""}
             </Button>
           </form>
 
@@ -145,7 +180,7 @@ const Pokemon = ({ openModal, onClose }) => {
               ) : (
                 <>
                   <span>
-                    <h1>{pokemonName}</h1>
+                    <h1>{pokemon.name}</h1>
                   </span>
 
                   <section>
@@ -154,17 +189,17 @@ const Pokemon = ({ openModal, onClose }) => {
                   </section>
 
                   <span className="specDetailsSpan">
-                    <section>
-                      <img
-                        src={pokemonworldmap}
-                        alt="Pokemon World Map"
-                        className="mapImg"
-                      />
-                      <img
-                        src={pokemon.image}
-                        alt=""
-                        className="nonMainImages"
-                      />
+                    <section
+                      style={{
+                        backgroundImage: "url(" + pokemonworldmap + ")",
+                        backgroundSize: "contain",
+                        backgroundRepeat: "no-repeat",
+                        width: "37rem",
+                        marginBottom: "-1.25rem",
+                      }}
+                    >
+                      <p className="horizontalLine"></p>
+                      <img src={pokemon.image} alt="" className="nonGifImage" />
 
                       <h2>
                         <span className="specDetails species">
