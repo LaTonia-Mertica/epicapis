528d20b13b5044d6bc8001b9850fc11946d6f001
La'Tonia Mertica <latoniamertica@gmail.com>
2022-11-03 15:29:24 -0400

    star wars: react select / refactor / functional

--- src/star wars/StarWars.jsx
+++ src/star wars/StarWars.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Modal from "@mui/material/Modal";
+import Select from "react-select";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
@@ -10,48 +11,112 @@ import { style } from "../mui.js";
 import stormtrooper from "./images/stormtrooper.gif";
 
 const StarWars = ({ openModal, onClose }) => {
-  const [starshipName, setStarshipName] = useState("");
+  // const [starshipName, setStarshipName] = useState("");
   const [starship, setStarship] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const [allStarships, setAllStarships] = useState([]);
 
-  const searchStarship = (event) => {
-    event.preventDefault();
+  const searchStarship = (starshipURL) => {
+    // event.preventDefault();
     setLoading(true);
-    axios
-      .get(`https://swapi.dev/api/starships/?search=${starshipName}`)
-      .then((response) => {
-        setStarship({
-          name: starshipName,
-          model: response.data.results[0].model,
-          class: response.data.results[0].starship_class,
-          manufacturer: response.data.results[0].manufacturer,
-          cost: response.data.results[0].cost_in_credits,
-          length: response.data.results[0].length,
-          crew: response.data.results[0].crew,
-          consumables: response.data.results[0].consumables,
-          capacityPassengers: response.data.results[0].passengers,
-          capacityCargo: response.data.results[0].cargo_capacity,
-          speed: response.data.results[0].max_atmosphering_speed,
-          hyperdrive: response.data.results[0].hyperdrive_rating,
-          megalights: response.data.results[0].MGLT,
-        });
-        setError(null);
-        setLoading(false);
-
-        setTimeout(() => {
-          setStarship(null);
-        }, 16000);
-      })
-      .catch((error) => {
-        console.error(error);
-        setError("No Such Starship!");
+    console.log(allStarships, starshipURL);
+    const matchingShip = allStarships.find((ship) => ship.url === starshipURL);
+    console.log(matchingShip);
+    if (matchingShip) {
+      setStarship({
+        name: matchingShip.name,
+        model: matchingShip.model,
+        class: matchingShip.starship_class,
+        manufacturer: matchingShip.manufacturer,
+        cost: matchingShip.cost_in_credits,
+        length: matchingShip.length,
+        crew: matchingShip.crew,
+        consumables: matchingShip.consumables,
+        capacityPassengers: matchingShip.passengers,
+        capacityCargo: matchingShip.cargo_capacity,
+        speed: matchingShip.max_atmosphering_speed,
+        hyperdrive: matchingShip.hyperdrive_rating,
+        megalights: matchingShip.MGLT,
       });
+      setTimeout(() => {
+        setStarship(null);
+      }, 15000);
+    } else {
+      setError("Starship Does Not Exist!");
+      setStarship(null);
+      setLoading(false);
+    }
+
+    setError(null);
+    setLoading(false);
+    setLoading(false);
+    // axios
+    //   .get(starshipURL)
+    //   .then((response) => {
+    //     console.log(response);
+    //     if (response?.data?.results?.name) {
+    //       console.log(response.data);
+    //       setStarship({
+    //         name: response.data.results[0].name,
+    //         model: response.data.results[0].model,
+    //         class: response.data.results[0].starship_class,
+    //         manufacturer: response.data.results[0].manufacturer,
+    //         cost: response.data.results[0].cost_in_credits,
+    //         length: response.data.results[0].length,
+    //         crew: response.data.results[0].crew,
+    //         consumables: response.data.results[0].consumables,
+    //         capacityPassengers: response.data.results[0].passengers,
+    //         capacityCargo: response.data.results[0].cargo_capacity,
+    //         speed: response.data.results[0].max_atmosphering_speed,
+    //         hyperdrive: response.data.results[0].hyperdrive_rating,
+    //         megalights: response.data.results[0].MGLT,
+    //       });
+    //     } else {
+    //       setError("Starship Does Not Exist!");
+    //       setStarship(null);
+    //       setLoading(false);
+    //     }
+
+    //     setError(null);
+    //     setLoading(false);
+
+    //     setTimeout(() => {
+    //       setStarship(null);
+    //     }, 16000);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     setError("Starship Does Not Exist!");
+    //     setLoading(false);
+    //   });
+  };
+
+  let starshipAggregator = [];
+
+  const getNextPage = (nextURL) => {
+    console.log(nextURL);
+    axios.get(nextURL).then((response) => {
+      const starshipData = response.data.results;
+
+      starshipAggregator = [...starshipAggregator, ...starshipData];
+
+      if (response.data.next) {
+        //recursive call if there is another page of data
+        getNextPage(response.data.next);
+      } else {
+        setAllStarships(starshipAggregator);
+        setLoading(false);
+      }
+    });
   };
 
   useEffect(() => {
-    if (!openModal) {
-      setStarship();
+    if (openModal) {
+      setLoading(true);
+      if (allStarships.length === 0) {
+        getNextPage(`https://swapi.dev/api/starships/?limit=1000`);
+      }
     }
   }, [openModal]);
 
@@ -73,10 +138,10 @@ const StarWars = ({ openModal, onClose }) => {
         <Box sx={style} className="starWarsCard">
           <div>
             <Button onClick={onClose}>&#x24B3;</Button>
-            <form onSubmit={searchStarship}>
-              {!starship && (
-                <>
-                  <input
+            <form>
+              {/* {!starship && (
+                <> */}
+              {/* <input
                     type="text"
                     name="starshipname"
                     id="starshipname"
@@ -85,17 +150,47 @@ const StarWars = ({ openModal, onClose }) => {
                     onChange={(event) => {
                       setStarshipName(event.target.value.toLowerCase());
                     }}
-                  />
+                  /> */}
 
-                  <Button
-                    type="submit"
-                    onClick={searchStarship}
-                    className="searchBtn"
-                  >
-                    {loading ? <>loading...</> : <>Search Starships</>}
-                  </Button>
-                </>
-              )}
+              <Select
+                options={allStarships.map((starship) => ({
+                  value: starship.url,
+                  label: starship.name,
+                }))}
+                styles={{
+                  option: () => ({
+                    color: "#545454",
+                    fontSize: "1.25rem",
+                    fontFamily: "monospace",
+                    textAlign: "left",
+                    paddingLeft: "1rem",
+                  }),
+                }}
+                onChange={(newValue) => {
+                  searchStarship(newValue.value);
+                }}
+                openMenuOnFocus={false}
+                openMenuOnClick={false}
+                placeholder="start typing starship name ..."
+                className="autocompleteSelect"
+              />
+
+              <Button
+                disabled={true}
+                // type="submit"
+                // onClick={searchStarship}
+                className="searchBtn"
+              >
+                {
+                  loading ? <>loading...</> : ""
+                  // (
+                  //   // <>Search Starships</>
+                  //   ""
+                  // )
+                }
+              </Button>
+              {/* </>
+              )} */}
               {!starship && (
                 <img
                   src={stormtrooper}
@@ -116,7 +211,8 @@ const StarWars = ({ openModal, onClose }) => {
                     <h1>{error}</h1>
                   ) : (
                     <>
-                      <h1>{starshipName}</h1>
+                      {/* <h1>{starshipName}</h1> */}
+                      <h1>{starship.name}</h1>
 
                       <h5>
                         <span className="specificationsText">model:&nbsp;</span>
