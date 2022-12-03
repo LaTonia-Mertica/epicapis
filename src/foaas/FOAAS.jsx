b95f480f7af37468b1d212881f7824eae3233ad0
Sheppard Walker <latoniamertica@gmail.com>
2022-12-03 14:04:07 -0500

    foaas: debugged / try catch w/randomized server-related error messages

--- src/foaas/FOAAS.jsx
+++ src/foaas/FOAAS.jsx
@@ -12,18 +12,49 @@ const FOAAS = ({ openModal, onClose }) => {
   const [foaas, setFoaas] = useState();
   const [loading, setLoading] = useState(false);
 
+  const errorMessages = [
+    "Oh My! Server Responded With Error.",
+    "Server Error. Try/Catch says 'Fuck Off'.",
+    "Guess Who's Been Heroku'ed!",
+    "Next Time, Maybe. Server in 'Oh Fuck' Mode.",
+    "Not Happening. Not My Fault. Not Just Saying.",
+    "Here We Are Waiting On a Server That Has No Fucks to Give.",
+    "Worry None. When Server Back Online, You'll Get the Effing Goods.",
+    "BONUS: Since You're Here, Refresh a Few Times. The FOAAS Cover Image is Randomized. That Sh#te is Fucking Awesome. Am I Right?!",
+  ];
+
   const getFoaas = async () => {
-    const option = options[Math.floor(Math.random() * options.length)];
-    setLoading(true);
-    const response = await fetch(
-      `https://foaas.com/${option}/Bad Code Day aka Ranting Baby`,
-      {
-        headers: { Accept: "application/json" },
+    try {
+      const option = options[Math.floor(Math.random() * options.length)];
+      setLoading(true);
+      const response = await fetch(
+        `https://foaas.com/${option}/Bad Code Day aka Ranting Baby`,
+        {
+          headers: { Accept: "application/json" },
+        }
+      );
+      const data = await response.json();
+      setFoaas(data);
+      setLoading(false);
+    } catch (error) {
+      let option =
+        errorMessages[Math.floor(Math.random() * errorMessages.length)];
+
+      //make sure the new error message is not the same as the currently displayed message, if it is, pick a different one
+      while (foaas?.message === option) {
+        option =
+          errorMessages[Math.floor(Math.random() * errorMessages.length)];
+        setFoaas({
+          message: option,
+          subtitle: "WHAT THE FUCK",
+        });
       }
-    );
-    const data = await response.json();
-    setFoaas(data);
-    setLoading(false);
+      setLoading(false);
+      setFoaas({
+        message: option,
+        subtitle: "WHAT THE FUCK",
+      });
+    }
   };
 
   useEffect(() => {
@@ -52,7 +83,7 @@ const FOAAS = ({ openModal, onClose }) => {
           {foaas && (
             <div>
               <h1>{foaas.message}</h1>
-              <p> {foaas.subtitle}&nbsp;-</p>
+              <p>-&nbsp;{foaas.subtitle}&nbsp;-</p>
             </div>
           )}
         </Box>
